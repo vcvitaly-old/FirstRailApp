789f3a5035436880b29bdacc4ee18dd0b37cf86e
Vitaly Chura <vcvitaly@yahoo.com>
2014-04-05 17:50:56 +0300

    Apperently finded the error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "foo@bar.com", password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -71,6 +70,16 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
